34d7ebd3b4ee6bc1fb5919acf3d66a0d643c45eb
Clark Batungbakal <batungbakalclark22@gmail.com>
2026-07-07 08:59:06 -0700

    Initial folders in the correct folder

---
+++ Assignment1.1.1/Program.cs
@@ -0,0 +1,11 @@
+﻿// This portion of the code prints out my name, age and address to the console
+
+Console.WriteLine("Please enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine("Please enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter your address: ");
+string address = Console.ReadLine();
+
+Console.WriteLine("My name is " + name + ", I am " + age +
+ " years old, and I live at " + address + ".\n");
