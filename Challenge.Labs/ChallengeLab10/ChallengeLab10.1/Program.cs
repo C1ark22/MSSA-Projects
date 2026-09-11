feffe507c00697407873cebdb634a8cf48fa3543
Clark Batungbakal <batungbakalclark22@gmail.com>
2026-09-10 23:28:47 -0700

    Week 10 Challenge lab added

---
+++ Challenge.Labs/ChallengeLab10/ChallengeLab10.1/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace ChallengeLab10._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abcd";
+            string t = "abcde";
+
+            char result = FindTheDifference(s, t);
+
+            Console.WriteLine($"The added letter is: {result}");
+        }
+        public static char FindTheDifference(string s, string t)
+        {
+            Dictionary<char, int> countS = new Dictionary<char, int>();
+            Dictionary<char, int> countT = new Dictionary<char, int>();
+
+            foreach (char letter in s)
+            {
+                if (countS.ContainsKey(letter))
+                {
+                    countS[letter]++;
+                }
+                else
+                {
+                    countS.Add(letter, 1);
+                }
+            }
+            foreach (char letter in t)
+            {
+                if (countT.ContainsKey(letter))
+                {
+                    countT[letter]++;
+                }
+                else
+                {
+                    countT.Add(letter, 1);
+                }
+            }
+            foreach (var pair in countT)
+            {
+                char letter = pair.Key;
+
+                if (!countS.ContainsKey(letter) || countT[letter] > countS[letter])
+                {
+                    return letter;
+                }
+
+            }
+            return '\0';
+        }
+    }
+}
---
+++ Challenge.Labs/ChallengeLab10/ChallengeLab10.2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ChallengeLab10._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+
+            int[] nums2 = { 2, 5, 6 };
+            int n = 3;
+
+            Merge(nums1, m, nums2, n);
+
+            Console.WriteLine(string.Join(", ", nums1));
+        }
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int i = m - 1;        // last real element in nums1
+            int j = n - 1;        // last element in nums2
+            int k = m + n - 1;    // last position in nums1
+
+            while (i >= 0 && j >= 0)
+            {
+                if (nums1[i] > nums2[j])
+                {
+                    nums1[k] = nums1[i];
+                    i--;
+                }
+                else
+                {
+                    nums1[k] = nums2[j];
+                    j--;
+                }
+
+                k--;
+            }
+
+            // If nums2 still has elements left,
+            // copy them into nums1
+            while (j >= 0)
+            {
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            }
+        }
+    }
+}
